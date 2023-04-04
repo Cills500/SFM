e08daf1af0035c5870c2743ecc6164fc0aca7715
Cills500 <129846491+Cills500@users.noreply.github.com>
2023-04-04 10:43:17 +0100

    Add files via upload

---
+++ matching_points_structureofmotion.m
@@ -0,0 +1,96 @@
+% Load left and right images
+Limg = imread('L_image.jpg');
+Rimg = imread('R_image.jpg');
+
+% Undistort images using precomputed camera intrinsics
+load 'calibrationSession3.mat'
+% Undistort images using precomputed camera intrinsics
+Limg = undistortImage(Limg, stereoParams3.CameraParameters1);
+Rimg = undistortImage(Rimg, stereoParams3.CameraParameters2);
+
+
+% Detect feature points in the left image
+imagePoints1 = detectMinEigenFeatures(rgb2gray(Limg), 'MinQuality', 0.1);
+
+% Visualize detected points
+figure
+imshow(Limg, 'InitialMagnification', 50);
+title('150 Strongest Corners from the Left Image');
+hold on
+plot(selectStrongest(imagePoints1, 150));
+
+% Create the point tracker
+tracker = vision.PointTracker('MaxBidirectionalError', 1, 'NumPyramidLevels', 5);
+
+% Initialize the point tracker
+imagePoints1 = imagePoints1.Location;
+initialize(tracker, imagePoints1, Limg);
+
+% Track the points in the right image
+[imagePoints2, validIdx] = step(tracker, Rimg);
+matchedPoints1 = imagePoints1(validIdx, :);
+matchedPoints2 = imagePoints2(validIdx, :);
+
+% Visualize correspondences
+figure
+showMatchedFeatures(Limg, Rimg, matchedPoints1, matchedPoints2);
+title('Tracked Features');
+
+intrinsics = stereoParams3.CameraParameters1;
+
+% Estimate the fundamental matrix
+[E, epipolarInliers] = estimateEssentialMatrix(...
+    matchedPoints1, matchedPoints2, intrinsics, 'Confidence', 99.99);
+
+% Find epipolar inliers
+inlierPoints1 = matchedPoints1(epipolarInliers, :);
+inlierPoints2 = matchedPoints2(epipolarInliers, :);
+
+% Display inlier matches
+figure
+showMatchedFeatures(Limg, Rimg, inlierPoints1, inlierPoints2);
+title('Epipolar Inliers');
+
+% Estimate relative pose
+relPose = relativeCameraPose(E, intrinsics, inlierPoints1, inlierPoints2);
+
+% Detect dense feature points in the left image
+border = 30;
+roi = [border, border, size(Limg, 2)- 2*border, size(Limg, 1)- 2*border];
+imagePoints1 = detectMinEigenFeatures(rgb2gray(Limg), 'ROI', roi, ...
+    'MinQuality', 0.001);
+
+% Initialize the point tracker
+imagePoints1 = imagePoints1.Location;
+%initialize(tracker, imagePoints1, Limg);
+
+% Track the points in the right image
+[imagePoints2, validIdx] = step(tracker, Rimg);
+matchedPoints1 = imagePoints1(validIdx, :);
+matchedPoints2 = imagePoints2(validIdx, :);
+
+% Compute the camera matrices for each position of the camera
+% The first camera is at the origin looking along the Z-axis. Thus, its
+% transformation is identity.
+camMatrix1 = cameraMatrix(intrinsics, eye(3), [0 0 0]);
+camMatrix2 = cameraMatrix(intrinsics, relPose.RotationMatrix, relPose.Translation.');
+
+% Compute the 3-D points
+points3D = triangulate(matchedPoints1, matchedPoints2, camMatrix1, camMatrix2);
+
+% Get the color of each reconstructed point
+numPixels = size(Limg, 1) * size(Limg, 2);
+allColors = reshape(Limg, [numPixels, 3]);
+colorIdx = sub2ind([size(Limg, 1), size(Limg, 2)], round(matchedPoints1(:,2)), round(matchedPoints1(:,1)));
+color = allColors(colorIdx, :);
+
+% Visualize the reconstructed 3-D points
+figure
+pcshow(points3D, color, 'VerticalAxis', 'Y', 'VerticalAxisDir', 'down', 'MarkerSize', 45);
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+title('Reconstructed 3-D Points');
+
+% Save the point cloud to a PLY file
+pcwrite(pointCloud(points3D, 'Color', color), 'pointcloud.ply');
